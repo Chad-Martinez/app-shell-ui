28cb7c03c04babfc4c88d3648ce33e07a7ad4a7e
Chad Martinez <chadcito@SomosLocos.local>
2023-06-07 01:42:15 -0600

    refactored login form to incorporate custom hook for validation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,12 @@
 export const validateEmailHelper = (email: string | undefined | null) => {
-  let isEmailError = true;
-  if (!email) return isEmailError;
+  let isValidEmail = false;
+  if (!email) return isValidEmail;
   if (
     email.match(
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     )
   ) {
-    isEmailError = false;
+    isValidEmail = true;
   }
-  return isEmailError;
+  return isValidEmail;
 };
